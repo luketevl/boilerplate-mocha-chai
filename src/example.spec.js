e92ed36400ab0301a6638c95314a8ae94850c16c
luketevl <luketevl@gmail.com>
2017-12-13 20:00:30 -0200

    news

--- src/example.spec.js
+++ src/example.spec.js
@@ -1,3 +1,9 @@
-const expect = require('chai').expect;
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
 
+chai.use(sinonChai);
+sinonStubPromise(sinon);
 
+global.fetch = require('node-fetch');
