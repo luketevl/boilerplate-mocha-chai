384206977147fab4f2db031a9dd18d26b3d1bfac
luketevl <luketevl@gmail.com>
2017-12-15 15:19:43 -0200

    added travis

--- src/example.spec.js
+++ src/example.spec.js
@@ -7,3 +7,9 @@ chai.use(sinonChai);
 sinonStubPromise(sinon);
 
 global.fetch = require('node-fetch');
+
+describe('Example', () => {
+  it('should x when y', () => {
+    expect(2).to.be.eql(2);
+  });
+});
